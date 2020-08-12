1b3b533357ffb469f2db6bb9e3ba0629dc85ad01
Arià Prat <a.prat@scrm.eu>
2020-08-12 15:46:09 +0200

    Add missing binding

--- app/src/main/java/com/aria/androiddaggerhilt/di/MainModule.kt
+++ app/src/main/java/com/aria/androiddaggerhilt/di/MainModule.kt
@@ -1,6 +1,6 @@
 package com.aria.androiddaggerhilt.di
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import com.aria.androiddaggerhilt.MainActivity
 import com.aria.androiddaggerhilt.MainContract
 import com.aria.androiddaggerhilt.MainPresenter
@@ -14,10 +14,8 @@ import dagger.hilt.android.components.ActivityComponent
 @Module
 abstract class MainModule {
 
-    /* Before I had:
     @Binds
     abstract fun bindActivity(activity: MainActivity): MainContract.View
-    */
 
     @Binds
     abstract fun bindPresenter(impl: MainPresenter): MainContract.Presenter
@@ -29,7 +27,7 @@ abstract class MainModule {
 object MainActivityModule {
 
     @Provides
-    fun bindActivity(activity: AppCompatActivity): MainActivity {
+    fun bindActivity(activity: Activity): MainActivity {
         return activity as MainActivity
     }
 }
