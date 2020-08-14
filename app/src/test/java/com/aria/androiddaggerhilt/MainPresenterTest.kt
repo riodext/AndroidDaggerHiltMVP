681663cb2edd7bc1359d24374dd323518364e641
Arià Prat <a.prat@scrm.eu>
2020-08-14 09:10:09 +0200

    Add MainPresenterTest

--- app/src/test/java/com/aria/androiddaggerhilt/MainPresenterTest.kt
+++ app/src/test/java/com/aria/androiddaggerhilt/MainPresenterTest.kt
@@ -1,4 +1,30 @@
 package com.aria.androiddaggerhilt
 
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
 class MainPresenterTest {
+    private val view: MainContract.View = mock()
+    private lateinit var presenter: MainPresenter
+
+    @BeforeEach
+    fun setUp(){
+        presenter = MainPresenter(view)
+    }
+
+    @DisplayName(
+        """
+            When view is created
+            Then show list
+        """
+    )
+    @Test
+    fun shouldShowList() {
+        presenter.onViewCreated()
+
+        verify(view).showList()
+    }
 }
