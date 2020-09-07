784317718b805e37392c7d5a30866a60245d2437
Joan <joancolmenero@gmail.com>
2020-09-07 10:38:01 +0200

    Adding network module

---
+++ network/src/main/java/com/aria/network/di/NetworkModule.kt
@@ -0,0 +1,50 @@
+package com.aria.network.di
+
+import com.aria.network.BuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@InstallIn(ApplicationComponent::class)
+@Module
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(httpClient: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(httpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(
+        httpLoggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient = OkHttpClient()
+        .newBuilder()
+        .addInterceptor(httpLoggingInterceptor)
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideHttpLogginInterceptor(): HttpLoggingInterceptor {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = when {
+            BuildConfig.DEBUG -> HttpLoggingInterceptor.Level.BODY
+            else -> HttpLoggingInterceptor.Level.NONE
+        }
+        return loggingInterceptor
+    }
+
+    private const val BASE_URL = "http://mysuperwebservice.com"
+
+}
+
